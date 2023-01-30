405376f33749baf9f8eb9601ef4124192c4ad738
Hunters565 <adrianagustinust@gmail.com>
2023-01-30 23:10:30 +0700

    view

--- Querries/tugas.sql
+++ Querries/tugas.sql
@@ -1,5 +1,5 @@
 -- KELOMPOK
-
+CREATE VIEW NilaiMAXMIN AS(
 SELECT SiswaMapel.Nama_Mapel, Nama_Siswa, Jenis_Kelamin, Nilai
 FROM (
 SELECT Nama_Mapel, MIN(Nilai) as MIN_Nilai, MAX(Nilai) as MAX_Nilai
@@ -9,9 +9,10 @@ GROUP BY Nama_Mapel
 JOIN SiswaMapel ON SiswaMapel.Nama_Mapel = SubjectGrades.Nama_Mapel
 AND (SiswaMapel.Nilai = SubjectGrades.MIN_Nilai OR SiswaMapel.Nilai = SubjectGrades.MAX_Nilai)
 JOIN Siswa ON Siswa.ID_Siswa = SiswaMapel.ID_Siswa
-
+)
 -- 2501969971 - Abel Kristanto
 -- Mencari makanan ter-banyak dan tersedikit
+CREATE VIEW FoodMAXMIN AS(
 SELECT * 
 FROM (
     SELECT Makanan.Nama_Makanan, y.ID_Makanan, y.ID_Count as Jumlah
@@ -26,27 +27,29 @@ WHERE Jumlah = (SELECT MIN(ID_Count) FROM (
 OR Jumlah = (SELECT MAX(ID_Count) FROM (
     SELECT ID_Makanan, COUNT(ID_Makanan) as ID_Count FROM `Transaksi` GROUP BY ID_Makanan
 ) as sub2)
-
+)
 
 -- 2501960814 - Augustinus Adrian Triputra
 -- Mencari guru yang mengajar terbanyak dan tersedikit
+CREATE VIEW TeachMAXMIN AS(
 SELECT Nama_Guru, Jenis_Kelamin, No_HP, COUNT(ID_Mapel) as Total_Mapel
 FROM Guru
 JOIN KelasMapel ON KelasMapel.ID_Guru = Guru.ID_Guru
 GROUP BY Guru.ID_Guru
 HAVING COUNT(ID_Mapel) = (
-                        SELECT MIN(MapelCount) FROM (
-                            SELECT COUNT(ID_Mapel) as MapelCount
-                            FROM Guru
-                            JOIN KelasMapel ON KelasMapel.ID_Guru = Guru.ID_Guru
-                            GROUP BY Guru.ID_Guru
-                            ) as GuruMapel
-                        ) 
+    SELECT MIN(MapelCount) FROM (
+        SELECT COUNT(ID_Mapel) as MapelCount
+        FROM Guru
+        JOIN KelasMapel ON KelasMapel.ID_Guru = Guru.ID_Guru
+        GROUP BY Guru.ID_Guru
+        ) as GuruMapel
+    ) 
 OR COUNT(ID_Mapel) = (
-                        SELECT MAX(MapelCount) FROM (
-                            SELECT COUNT(ID_Mapel) as MapelCount
-                            FROM Guru
-                            JOIN KelasMapel ON KelasMapel.ID_Guru = Guru.ID_Guru
-                            GROUP BY Guru.ID_Guru
-                            ) as GuruMapel
-                        );
+    SELECT MAX(MapelCount) FROM (
+        SELECT COUNT(ID_Mapel) as MapelCount
+        FROM Guru
+        JOIN KelasMapel ON KelasMapel.ID_Guru = Guru.ID_Guru
+        GROUP BY Guru.ID_Guru
+        ) as GuruMapel
+    )
+)
